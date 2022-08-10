58a06ba4b0a2e9878eaabcb49f56a5f11c286156
VeraMaminova <maminovavv@yandex.ru>
2022-08-10 20:48:54 +0300

    Add gitignore

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает последнюю цифру этого числа.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int userNum = Prompt("Введите 3-х значное число > ");
+
+int result = userNum /10 % 10;
+Console.WriteLine(result);
