07810438a7563e461423d8c81387f57f5feb0f0d
VeraMaminova <maminovavv@yandex.ru>
2022-08-10 22:31:36 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int second = 10 * ( number / 100 )  + ( number % 10 ); 
+Console.WriteLine(second);
